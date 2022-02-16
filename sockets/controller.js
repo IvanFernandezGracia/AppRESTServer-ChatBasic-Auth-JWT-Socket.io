7f999a43fcefaf3794aa9a014a2047eb6e5023e9
Ivan Alejandro <ivan.fernandez.g@usach.cl>
2022-02-16 12:16:45 -0300

    v1

---
+++ sockets/controller.js
@@ -0,0 +1,43 @@
+const { Socket } = require("socket.io");
+const { comprobarJWT } = require("../helpers");
+const { ChatMensajes } = require("../models");
+
+const chatMensajes = new ChatMensajes();
+
+const socketController = async (socket = new Socket(), io) => {
+  const usuario = await comprobarJWT(socket.handshake.headers["x-token"]);
+  if (!usuario) {
+    return socket.disconnect();
+  }
+
+  // Agregar el usuario conectado
+  chatMensajes.conectarUsuario(usuario);
+  io.emit("usuarios-activos", chatMensajes.usuariosArr);
+  socket.emit("recibir-mensajes", chatMensajes.ultimos10);
+
+  // Conectarlo a una sala especial
+  // Socket.id: Problema puede crear dos socket con el mismo id si esta en dos navegadores
+  // global, socket.id, usuario.id
+  socket.join(usuario.id); // id unico del usuario en mongo DB
+  // socket.join(socket.id);
+
+  // Limpiar cuando alguien se desconeta
+  socket.on("disconnect", () => {
+    chatMensajes.desconectarUsuario(usuario.id);
+    io.emit("usuarios-activos", chatMensajes.usuariosArr);
+  });
+
+  socket.on("enviar-mensaje", ({ uid, mensaje }) => {
+    if (uid) {
+      // Mensaje privado
+      socket.to(uid).emit("mensaje-privado", { de: usuario.nombre, mensaje });
+    } else {
+      chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
+      io.emit("recibir-mensajes", chatMensajes.ultimos10);
+    }
+  });
+};
+
+module.exports = {
+  socketController,
+};
